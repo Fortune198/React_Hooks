e29a328cba52d45789343de9900c8ac6ec5bf2ed
Fortune198 <fortunematshika@gmail.com>
2024-11-05 16:17:49 +0200

    Updated

---
+++ chapter06/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import UserForm from "./UserForm";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+
+  return (
+    <div>
+      <UserForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ chapter07/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import GitHub from "./GitHub";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+
+  return (
+    <div>
+      <GitHub />
+    </div>
+  );
+}
+
+export default App;
