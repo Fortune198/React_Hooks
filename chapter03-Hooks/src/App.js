20160660bd12a85197f2bdc231dd31cac87c63ac
Fortune198 <fortunematshika@gmail.com>
2024-11-08 00:23:54 +0200

    updated

---
+++ chapter03-Hooks/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Products from "./Products";
+import Rating from './Rating';
+import { Button } from 'react-bootstrap';
+
+
+function App() {
+  //const isValid = true;
+
+  return (
+    <div>
+      {/* <Products />
+      <Button variant="danger" disable={!isValid}>Default</Button> */}
+      <Rating rating='1' />
+      <Rating rating='2' />
+      <Rating rating='3' />
+      <Rating rating='4' />
+      <Rating rating='5' />
+    </div>
+  );
+}
+
+export default App;
